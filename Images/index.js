132d01ffd701fbfc73b46ee4c8a675cb0c426079
M-Alpha199 <mac.programmer.millan@gmail.com>
2025-03-06 21:34:57 +0000

    second commit

---
+++ Images/index.js
@@ -0,0 +1,10 @@
+
+let username;
+/*document.getElementsById("button").onclick = function(){
+
+   username =  document.getElementById("input").value;
+   document.getElementById("header").textContent = "Hey ${username}";
+  //console.log(username);
+}
+  */
+ console.log("Hello World");
